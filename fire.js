1181eb6fbd0886a8fcccd5f07e92b69eceee60b8
Caio R. Vieira <caio.robs98@gmail.com>
2021-07-25 15:31:42 -0300

    fire database set

--- fire.js
+++ fire.js
@@ -1,11 +1,16 @@
 const pixelsArray = [];
+const fireWidth = 10;
+const fireHeight = 10;
 
 function start() {
 
 }
 
 function createFireDataStructure() {
-
+	const numberOfPixels = fireWidth * fireHeight;
+	for (let i = 0; i < numberOfPixels; i++){
+		pixelsArray[i] = 0;
+	}
 }
 
 function calculateFirePropagation() {
