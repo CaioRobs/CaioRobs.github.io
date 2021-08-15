6f02c61420a953ca838498746156f4fcf2842129
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-15 16:19:47 -0300

    links pixel art project in projects area

--- scriptsJS/script-pixel-art.js
+++ scriptsJS/script-pixel-art.js
@@ -1,113 +1,117 @@
 function handlePixelClick(event) {
-  const pixel = event.target;
-  const selectedColorInPallette = document.querySelector('.selected');
-  const selectedColorComputedStyle = window.getComputedStyle(selectedColorInPallette, null);
-  const selectedColor = selectedColorComputedStyle.getPropertyValue('background-color');
-  pixel.style.backgroundColor = selectedColor;
+	const pixel = event.target;
+	const selectedColorInPallette = document.querySelector('.selected');
+	const selectedColorComputedStyle = window.getComputedStyle(selectedColorInPallette, null);
+	const selectedColor = selectedColorComputedStyle.getPropertyValue('background-color');
+	pixel.style.backgroundColor = selectedColor;
 }
 
 function addListenerToPixels() {
-  const pixels = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', handlePixelClick);
-  }
+	const pixels = document.getElementsByClassName('pixel');
+	for (let index = 0; index < pixels.length; index += 1) {
+		pixels[index].addEventListener('click', handlePixelClick);
+	}
 }
 
 function fillNewBoard(newBoard, n) {
-  for (let index = 1; index <= (n * n); index += 1) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    newBoard.appendChild(pixel);
-    if (index % n === 0) {
-      newBoard.appendChild(document.createElement('br'));
-    }
-  }
+	for (let index = 1; index <= (n * n); index += 1) {
+		const pixel = document.createElement('div');
+		pixel.classList.add('pixel');
+		newBoard.appendChild(pixel);
+		if (index % n === 0) {
+			newBoard.appendChild(document.createElement('br'));
+		}
+	}
 }
 
 function createNewBoard() {
-  const newBoard = document.createElement('section');
-  newBoard.id = ('pixel-board');
-  return newBoard;
+	const newBoard = document.createElement('section');
+	newBoard.id = ('pixel-board');
+	return newBoard;
 }
 
 function produceBoard(size) {
-  const newBoard = createNewBoard();
-  fillNewBoard(newBoard, size);
-  document.getElementsByTagName('main')[0].appendChild(newBoard);
-  addListenerToPixels();
+	const newBoard = createNewBoard();
+	fillNewBoard(newBoard, size);
+	document.getElementsByTagName('main')[0].appendChild(newBoard);
+	addListenerToPixels();
 }
 
 function deleteOldBoard() {
-  const oldBoard = document.querySelector('#pixel-board');
-  oldBoard.remove();
+	const oldBoard = document.querySelector('#pixel-board');
+	oldBoard.remove();
 }
 
 function verifyInput() {
-  const input = document.querySelector('#board-size');
-  const n = parseInt(input.value, 10);
+	const input = document.querySelector('#board-size');
+	const n = parseInt(input.value, 10);
 
-  if (Number.isNaN(parseInt(input.value, 10))) {
-    return false;
-  }
-  if (n <= 5) {
-    return 5;
-  }
-  if (n >= 50) {
-    return 50;
-  }
-  return n;
+	if (Number.isNaN(parseInt(input.value, 10))) {
+		return false;
+	}
+	if (n <= 5) {
+		return 5;
+	}
+	if (n >= 50) {
+		return 50;
+	}
+	return n;
 }
 
 function handleGenerateBtnClick() {
-  const inputValue = verifyInput();
-  if (inputValue === false) {
-    return alert('Board inválido!');
-  }
-  deleteOldBoard();
-  produceBoard(inputValue);
+	const inputValue = verifyInput();
+	if (inputValue === false) {
+		return alert('Board inválido!');
+	}
+	deleteOldBoard();
+	produceBoard(inputValue);
 }
 
 function handleClearBtnClick() {
-  const pixels = document.querySelectorAll('.pixel');
+	const pixels = document.querySelectorAll('.pixel');
 
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
-  }
+	for (let index = 0; index < pixels.length; index += 1) {
+		pixels[index].style.backgroundColor = 'white';
+	}
 }
 
 function handleColorClick(event) {
-  const currentSelected = document.querySelector('.selected');
-  const newSelected = event.target;
-  currentSelected.classList.remove('selected');
-  newSelected.classList.add('selected');
+	const currentSelected = document.querySelector('.selected');
+	const newSelected = event.target;
+	currentSelected.classList.remove('selected');
+	newSelected.classList.add('selected');
 }
 
 function addListenerToColorPallete() {
-  const colors = document.getElementsByClassName('color');
-  for (let index = 0; index < colors.length; index += 1) {
-    colors[index].addEventListener('click', handleColorClick);
-  }
+	const colors = document.getElementsByClassName('color');
+	for (let index = 0; index < colors.length; index += 1) {
+		colors[index].addEventListener('click', handleColorClick);
+	}
 }
 
 function randomizeColors() {
-  const colors = document.getElementsByClassName('color');
-  for (let index = 1; index < colors.length; index += 1) {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    colors[index].style.backgroundColor = `#${randomColor}`;
-  }
+	const colors = document.getElementsByClassName('color');
+	for (let index = 1; index < colors.length; index += 1) {
+		const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		colors[index].style.backgroundColor = `#${randomColor}`;
+	}
 }
 
 function load() {
-  randomizeColors();
-  addListenerToColorPallete();
+	const body = document.querySelector('body')
+	console.log(window.innerHeight)
+	body.style.height = window.innerHeight + 'px';
 
-  const clearBtn = document.getElementById('clear-board');
-  clearBtn.addEventListener('click', handleClearBtnClick);
+	randomizeColors();
+	addListenerToColorPallete();
 
-  const generateBtn = document.querySelector('#generate-board');
-  generateBtn.addEventListener('click', handleGenerateBtnClick);
+	const clearBtn = document.getElementById('clear-board');
+	clearBtn.addEventListener('click', handleClearBtnClick);
 
-  produceBoard(5);
+	const generateBtn = document.querySelector('#generate-board');
+	generateBtn.addEventListener('click', handleGenerateBtnClick);
+
+	produceBoard(5);
 }
 
 window.onload = load;
