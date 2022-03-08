4ce89e9edc03b053ed912cb912911f900702833c
Caio R. Vieira <caio.robs98@gmail.com>
2022-03-08 20:21:36 -0300

    refactoring ts way

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { NextPage } from "next";
 import { Title } from "../styles/Title";
 
-export default function Home() {
+const Home: NextPage = () => {
   return <Title>My page</Title>;
-}
+};
+
+export default Home;
