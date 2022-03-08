d6dc77978b35738516ab50b71834e3a5a3446b4b
Caio R. Vieira <caio.robs98@gmail.com>
2022-03-08 20:10:38 -0300

    next project with tailwind and styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "styled-components";
+import "../styles/globals.css";
+import { GlobalStyle } from "../styles/GlobalStyle";
+import { theme } from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
